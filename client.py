d6732f5174567b3eed6e49a2f05f1aae9344642d
kanagasabapathi <kanagasabapathi@hotmail.com>
2015-03-20 11:25:41 +0530

    Update client.py
    When client start communicate give the input and the program is executing like that
    
    
    Connected to remote host. Start sending messages
    <You> 101 5 6 2 3 1 0
    Enter p,q1,q2,r1,r2,b1,b2  c1 =  510
    c2 =  612
    Decrypt= 11
    b= 1 value is got for addition
    b value is odd
    b= 0 value is got for multiplication
    b value is even

--- client.py
+++ client.py
@@ -8,26 +8,28 @@ def prompt():
    sys.stdout.flush()
 
 HOST = '127.0.0.1'
-PORT = 6746
+PORT = 8096
+
+
 try:
-  client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.error:
     print 'Failed to create socket'
     sys.exit()
 
-client_socket.connect((HOST, PORT))
+s.connect((HOST, PORT))
 
 print 'Connected to remote host. Start sending messages'
 prompt()
 
-while 1:
+while  1:
+    
 
-    socket_list = [sys.stdin, client_socket]
+    socket_list = [sys.stdin, s]
 
     read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
-
     for sock in read_sockets:
-        if sock == client_socket:
+        if sock == s:
             data = sock.recv(4096)
             if not data:
                 print '\nDisconnected from chat server'
@@ -36,55 +38,47 @@ while 1:
                 sys.stdout.write(data)
                 prompt()
         else:
-            msg = sys.stdin.readline()
-            client_socket.send('\r<Client>: ' + msg)
-            prompt()
-	    p,q1,q2=map(int,raw_input("enter p,q1,q2").split())
-	    r1,r2=map(int,raw_input("enter r1,r2").split()) 
-	    b1,b2=map(int,raw_input("enter b1,b2").split())
-	    c1=(p*q1)+(2*r1+b1)
-	    c2=(p*q2)+(2*r2+b2)
-	    print"c1=",c1
-	    print"c2=",c2
-	    client_socket.send(str(c1)+":"+str(c2))
-	    sm=(c1+c2)%p
-	    mult=(c1*c2)%p
+	    p,q1,q2,r1,r2,b1,b2 = map(int,raw_input("Enter p,q1,q2,r1,r2,b1,b2  ").split())
+  	    c1=p*q1+2*r1+b1
+  	    c2=p*q2+2*r2+b2
+	    print "c1 = ",c1
+	    print "c2 = ",c2
+	    #msg = sys.stdin.readline()
+	    
+            s.send(str(c1)+":"+str(c2))
+	    #s.send(str(c2))
+
+            sum1=(c1+c2)%p
+	    multiplication=(c1*c2)%p
             
 
 
 	  #  client_socket.send(str(sm))
 
 
-	    print"decrypt=",sm
-	    if sm%2==0:
+	    print"Decrypt=",sum1
+	    if sum1%2==0:
 	   	         b=0
 	    else:
 	  	         b=1
 
 	    
-            print"b=",b,"for addition"
+            print"b=",b,"value is got for addition"
 	    if b==0:
-	                 print "noise is even"
+	                 print "b value is even"
 	    else:
-	                 print "noise is odd"
+	                 print "b value is odd"
  	        
-	    if mult%2==0:
+	    if multiplication%2==0:
 	                 b=0
 	    else:
 			 b=1
-	    print"b=",b,"for multiplication"
+	    print"b=",b,"value is got for multiplication"
 	    
 	    if b==0:
-                         print "noise is even"
+                         print "b value is even"
             else:
-                         print "noise is odd"
-
-
-
-
-	   # client_socket.send(str(b))
-
-
-
-
-
+                         print "b value is odd"
+	    
+	  
+            prompt()
